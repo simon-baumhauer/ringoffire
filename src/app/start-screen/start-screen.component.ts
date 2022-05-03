cbe3fadca086df03626b17a4e48f4e087399c6bb
simon baumhauer <simon.baumhauer@hotmail.ch>
2022-05-03 11:09:41 +0200

    router for newgame added

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
-  styleUrls: ['./start-screen.component.scss']
+  styleUrls: ['./start-screen.component.scss'],
 })
 export class StartScreenComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  newGame() {
+    this.router.navigateByUrl('/game');
   }
-
 }
